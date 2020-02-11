d107d7351eb9ded9702370048176f46d62f0ca12
Boris Pismenny <BorisPismenny@gmail.com>
2020-02-11 23:25:16 +0200

    http.lua: select a random file in dante732 1/630000 files
    Signed-off-by: Boris Pismenny <BorisPismenny@gmail.com>

---
+++ http.lua
@@ -0,0 +1,78 @@
+-- Resource: https://github.com/timotta/wrk-scripts/blob/master/multiplepaths.lua
+-- initialize the pseudo random number generator
+-- Resource: http://lua-users.org/wiki/MathLibraryTutorial
+math.randomseed(os.time())
+math.random(); math.random(); math.random()
+
+-- Shuffle array
+-- Returns a randomly shuffled array
+function shuffle(paths)
+	local j, k
+	local n = #paths
+
+	for i = 1, n do
+		j, k = math.random(n), math.random(n)
+		paths[j], paths[k] = paths[k], paths[j]
+	end
+
+	return paths
+end
+
+-- Load URL paths from the file
+function load_url_paths_from_file(file)
+	lines = {}
+
+	-- Check if the file exists
+	-- Resource: http://stackoverflow.com/a/4991602/325852
+	local f=io.open(file,"r")
+	if f~=nil then 
+		io.close(f)
+	else
+		-- Return the empty array
+		return lines
+	end
+
+	-- If the file exists loop through all its lines 
+	-- and add them into the lines array
+	for line in io.lines(file) do
+		if not (line == '') then
+			lines[#lines + 1] = line
+		end
+	end
+
+	--return shuffle(lines)
+	return lines
+end
+
+-- Load URL paths from file
+--paths = load_url_paths_from_file("urls.txt")
+MAX_PATHS=630000
+
+-- Check if at least one path was found in the file
+--if #paths <= 0 then
+--	print("multiplepaths: No paths found. You have to create a file paths.txt with one path per line")
+--	os.exit()
+--end
+
+--print("multiplepaths: Found " .. #paths .. " paths")
+
+-- Initialize the paths array iterator
+counter = 0
+
+request = function()
+	-- Get the next paths array element
+	--url_path = paths[counter]
+	url_path = string.format("http://10.1.4.100/file.%d.html", math.random(MAX_PATHS))
+
+	-- 
+	counter = counter + 1
+
+	-- If the counter is longer than the paths array length then reset it
+	--if counter > #paths then
+	if counter > MAX_PATHS then
+		counter = 0
+	end
+
+	-- Return the request object with the current URL path
+	return wrk.format(nil, url_path)
+end
